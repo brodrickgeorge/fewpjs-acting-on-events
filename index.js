af60ba3e7ddaeacd2b83fa57a34e580165827668
brodrickgeorge <brodrick.george@yahoo.com>
2020-12-12 22:58:30 -0600

    commit tree 61097ee8fca4b8d94f163aa3c848f618b949f1c4

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Your code here
+var dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+
+/*
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+console.log(dodger.style.left); //
+("180px");
+console.log(dodger.style.bottom); //
+("0px");
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 5}px`;
+  }
+  
+});
+*/
